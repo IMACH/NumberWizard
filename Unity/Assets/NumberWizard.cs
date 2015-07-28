314506d23117db30ce231b3f04df0ed641d7d184
IMACH <mr.kevinmach@gmail.com>
2015-07-28 14:21:25 +0800

    Game Loop
    when game  is finished, automatically restart the game.

--- Unity/Assets/NumberWizard.cs
+++ Unity/Assets/NumberWizard.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
-	int max = 1000; 
-	int min = 1; 
-	int guess = 500; 
+	int max; 
+	int min; 
+	int guess; 
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +13,13 @@ public class NumberWizard : MonoBehaviour {
 	}
 
 	void StartGame(){
-		max = max + 1;
+		max = 1000; 
+		min = 1; 
+		guess = 500; 
+
+
+
+		print ("==========================="); 
 		print ("Welcome to Number Wizard"); 
 		print ("Pick a number in your head, but don't tell me");
 		
@@ -22,6 +28,8 @@ public class NumberWizard : MonoBehaviour {
 		
 		print ("Is the number higher or lower than " + guess + "?"); 
 		print ("Up = higher, Down = lower, Return = equals"); 
+
+		max = max + 1;
 	}
 
 
@@ -38,6 +46,7 @@ public class NumberWizard : MonoBehaviour {
 			NextGuess (); 
 		} else if (Input.GetKeyDown (KeyCode.Return)) {
 			print ("I won!"); 
+			StartGame (); 
 	}
 
 	
